8b0dcb52c7e9f3791467dc3c81d146235ea50ca4
Keshav Kumar <keshavkumar8122@gmail.com>
2022-10-25 11:39:47 +0530

    Added signup page
    Added signup page    25 October

---
+++ signUp/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('#smartwizard').smartWizard({
+        selected: 0,
+        theme: 'arrows',
+        autoAdjustHeight: true,
+        transitionEffect: 'fade',
+        showStepURLhash: false,
+
+    });
+
+});
